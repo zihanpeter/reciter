da5c8a028e1fd2042a7b8e3b42f211b31a133df9
zihanpeter <luzihan1104@163.com>
2024-12-10 20:16:08 +0800

    1.4

--- defender.py
+++ defender.py
@@ -9,7 +9,7 @@ def generate_captcha():
     letters = string.ascii_letters
     captcha_text = ''.join(random.choice(letters) for i in range(4))
     # 创建图片验证码
-    image = ImageCaptcha(width=90, height=45)
+    image = ImageCaptcha(width=90, height=50)
     captcha_image = image.generate(captcha_text)
     # 将验证码图片以二进制形式写入内存
     captcha_binary = base64.b64encode(captcha_image.getvalue()).decode('utf-8')
